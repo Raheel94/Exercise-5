1ed8e7307811fdba88ffa6201f2edd354f02c6d8
Raheel94 <muha.raheel94@gmail.com>
2023-12-03 11:06:13 +0500

    initial message

---
+++ index.ts
@@ -0,0 +1,15 @@
+// Exercise 5:  Write a program that determines if a person is eligible to vote based on their age.
+
+function eligibleToVote(num:number): string {
+    if(num >= 18){
+        return 'eligible to vote';
+    } else {
+        return 'not eligible to vote';
+    }
+}
+
+//Test the function 
+
+let age = 11
+let result = eligibleToVote(age);
+console.log(`The person is ${result}`)
